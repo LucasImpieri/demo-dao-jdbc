01e5870c80632411ab533166c427a8c31e382287
Lucas Impieri <lucasimpieri@gmail.com>
2020-07-16 13:35:09 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,5 +27,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n===== TEST 3: seller findByAll =====");
+		System.out.println();
+		list = sellerDao.findAll();
+		
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 	}
 }
